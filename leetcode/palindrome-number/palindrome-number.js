6165804b48f1cbbf662498f22d8b7b1da6bbeea2
Marc Ribas <ribascode@gmail.com>
2025-03-17 17:38:01 +0100

    Add README.md, index.html, and palindrome-number.js for palindrome number problem

---
+++ leetcode/palindrome-number/palindrome-number.js
@@ -0,0 +1,17 @@
+
+'use strict' // https://leetcode.com/problems/palindrome-number
+
+// Case 1
+let x = 121
+
+// Case 2
+// let x = -121
+
+// Case 3
+// let x = 10
+
+function isPalindrome(x) {
+
+}
+
+console.log(isPalindrome(x))
