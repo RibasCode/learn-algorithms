2aaa09cefc5d362d107bcc2841b9f6a06f4a2b81
Marc Ribas <ribascode@gmail.com>
2025-03-17 17:45:17 +0100

    Implement palindrome check using string reversal in palindrome-number.js

--- leetcode/palindrome-number/palindrome-number.js
+++ leetcode/palindrome-number/palindrome-number.js
@@ -11,7 +11,15 @@ let x = 121
 // let x = 10
 
 function isPalindrome(x) {
+  let word = x.toString();
+  let wordLength = word.length - 1;
+  let reversedWord = '';
 
+  for(let i = wordLength; i >= 0; i--){
+    reversedWord += word[i];
+  }
+
+  return word === reversedWord
 }
 
 console.log(isPalindrome(x))
