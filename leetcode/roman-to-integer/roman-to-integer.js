ba6eaa5f73e140c6ca9dadcf09cc69adb16849b9
Marc Ribas <ribascode@gmail.com>
2025-08-25 17:56:23 +0200

    Fix typo in strict mode declaration and refactor romanToInt function for clarity

--- leetcode/roman-to-integer/roman-to-integer.js
+++ leetcode/roman-to-integer/roman-to-integer.js
@@ -1,5 +1,5 @@
 
-'use strict' // https://leetcode.com/problems/roman-to-integer
+'use sict' // https://leetcode.com/problems/roman-to-integer
 
 // Case 1
 //Input: s = "III"
@@ -16,9 +16,96 @@
 // Output: 1994
 // Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
-function romanToInt(string) {
+let s = "LVIII"
 
+function romanToInt(s) {
 
+  let counter = 0
+
+  for(let i = 0; i < s.length; i++) {
+
+    if(s[i] === "I") {
+
+      if(s[i+1] !== "I" && s[i+1] !== undefined) {
+        counter -= 1
+        continue
+      }
+
+      counter += 1
+    }
+
+    if(s[i] === "V") {
+
+      if(
+          s[i+1] === "L" && s[i+1] !== undefined ||
+          s[i+1] === "C" && s[i+1] !== undefined ||
+          s[i+1] === "D" && s[i+1] !== undefined ||
+          s[i+1] === "M" && s[i+1] !== undefined
+        ) {
+        counter -= 5
+        continue
+      }
+
+      counter += 5
+    }
+
+    if(s[i] === "X") {
+
+      if(
+        s[i+1] === "L" && s[i+1] !== undefined ||
+        s[i+1] === "C" && s[i+1] !== undefined ||
+        s[i+1] === "D" && s[i+1] !== undefined ||
+        s[i+1] === "M" && s[i+1] !== undefined
+      ) {
+        counter -= 10
+        continue
+      }
+
+      counter += 10
+    }
+
+    if(s[i] === "L") {
+
+      if(
+        s[i+1] === "D" && s[i+1] !== undefined ||
+        s[i+1] === "M" && s[i+1] !== undefined
+      ) {
+        counter -= 50
+        continue
+      }
+
+      counter += 50
+    }
+
+    if(s[i] === "C") {
+
+      if(
+        s[i+1] === "D" && s[i+1] !== undefined ||
+        s[i+1] === "M" && s[i+1] !== undefined
+      ) {
+        counter -= 100
+        continue
+      }
+
+      counter += 100
+    }
+
+    if(s[i] === "D") {
+
+      if(s[i+1] === "M" && s[i+1] !== undefined) {
+        counter -= 500
+        continue
+      }
+
+      counter += 500
+    }
+
+    if(s[i] === "M") {
+      counter += 1000
+    }
+  }
+
+  return counter
 }
 
-console.log(romanToInt(string))
+console.log(romanToInt(s))
